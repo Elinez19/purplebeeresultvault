5a4d6ed55896382cc588d3847206c6605f607d98
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-07-15 09:22:06 +0100

    PBRESULTVAULT LANDING PAGE DESIGN AND DEVELOPEMENT

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -30,13 +30,13 @@ const Footer = () => {
                         <img src={'/images/pbresult-logo.png'} alt="logo" className='pb-8' />
                         <div className='flex gap-4'>
                             <Link href="https://facebook.com" className='Footer-fb-icons'>
-                                <Image src={'/images/Footer/facebook.svg'} alt="facebook" width={15} height={20} />
+                                <Image src={'/images/footer/facebook.svg'} alt="facebook" width={15} height={20} />
                             </Link>
                             <Link href="https://twitter.com" className='Footer-icons'>
-                                <Image src={'/images/Footer/twitter.svg'} alt="twitter" width={20} height={20} />
+                                <Image src={'/images/footer/twitter.svg'} alt="twitter" width={20} height={20} />
                             </Link>
                             <Link href="https://instagram.com" className='Footer-icons'>
-                                <Image src={'/images/Footer/instagram.svg'} alt="instagram" width={20} height={20} />
+                                <Image src={'/images/footer/instagram.svg'} alt="instagram" width={20} height={20} />
                             </Link> 
                         </div>
                     </div>
@@ -59,15 +59,15 @@ const Footer = () => {
 
                     <div className='col-span-4 md:col-span-4 lg:col-span-4'>
                         <div className="flex gap-2">
-                            <Image src={'/images/Footer/mask.svg'} alt="mask-icon" width={24} height={24} />
+                            <Image src={'/images/footer/mask.svg'} alt="mask-icon" width={24} height={24} />
                             <h5 className="text-base font-normal text-offwhite">No 41, Ojido street Beside Ojido market, Ado- Ekiti</h5>
                         </div>
                         <div className="flex gap-2 mt-10">
-                            <Image src={'/images/Footer/telephone.svg'} alt="telephone-icon" width={24} height={24} />
+                            <Image src={'/images/footer/telephone.svg'} alt="telephone-icon" width={24} height={24} />
                             <h5 className="text-base font-normal text-offwhite">+2349122205393</h5>
                         </div>
                         <div className="flex gap-2 mt-10">
-                            <Image src={'/images/Footer/email.svg'} alt="email-icon" width={24} height={24} />
+                            <Image src={'/images/footer/email.svg'} alt="email-icon" width={24} height={24} />
                             <h5 className="text-base font-normal text-offwhite">info@purplebeetech.com</h5>
                         </div>
                     </div>
